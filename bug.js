20401c5ee7f03240e6a9fe4757916d2cf76de3b8
BugHunter-X <askzenai@gmail.com>
2024-12-13 01:32:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,22 @@
+```javascript
+import React, { useState, useEffect } from 'react';
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Incorrect way to update count in useEffect
+    count = count + 1; //This will not trigger re-render
+    setCount(count + 1); //This works but not as expected
+  }, []);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+
+export default MyComponent;
+```
