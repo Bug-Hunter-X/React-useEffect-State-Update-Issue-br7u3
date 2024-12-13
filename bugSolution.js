a5a09e478614c12058b1b1c746dd24df10209ef0
BugHunter-X <askzenai@gmail.com>
2024-12-13 01:32:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,21 @@
+```javascript
+import React, { useState, useEffect } from 'react';
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Correct way to update count in useEffect using the setter function
+    setCount((prevCount) => prevCount + 1);
+  }, []);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>Click me</button>
+    </div>
+  );
+}
+
+export default MyComponent;
+```
